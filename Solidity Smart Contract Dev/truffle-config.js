0c093871afe8aab4e89e5497ae452a611c549aef
David Ramirez <ramirdav@oregonstate.edu>
2018-03-31 00:22:13 -0400

    Update truffle-config.js

--- Solidity Smart Contract Dev/truffle-config.js
+++ Solidity Smart Contract Dev/truffle-config.js
@@ -1,9 +1,32 @@
+var HDWalletProvider = require("truffle-hdwallet-provider")
+
+var mnemonic = ""
+
+
 module.exports = {
+
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      gas: 8000000
+    },
+     ropsten:  {
+     provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+     network_id: 3,
+     //host: "localhost",
+     //port:  8545,
+     gas: 4700000,
+     gasPrice: 4000000000
+     },
+     main:  {
+     provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/"),
+     network_id: 1,
+     //host: "localhost",
+     //port:  8545,
+     gas: 2000000,
+     gasPrice: 4000000000
+      }
     }
-  }
-};
+  };
