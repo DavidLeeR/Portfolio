4363da146a9e9543e9bb7b202c0f6cbd36c68eb5
David Ramirez <ramirdav@oregonstate.edu>
2019-06-05 21:26:36 -0400

    clean up

--- Web Development/Blockchain_Info_Webapp/dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_ramirdav',
-  password        : '071591',
-  database        : 'cs340_ramirdav'
-});
-module.exports.pool = pool;
--- Web Development/README.md
+++
@@ -1,6 +0,0 @@
-**Web Development Projects**
-
-**Web projects created using:**
-Languages: HTML, CSS, mySQL, JavaScript
-Frameworks: Express.js, Handlebars.js
-                                                                                 
